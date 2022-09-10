4a5ead3b2ae16910f4e8081df29fd3fdfa6faacd
Adipiter <miuprix@gmail.com>
2022-09-10 07:38:10 +0700

    add form
    Signed-off-by: Adipiter <miuprix@gmail.com>

--- app/Views/login.php
+++ app/Views/login.php
@@ -5,45 +5,32 @@
 <div class="login-box">
     <div class="login-box-body">
         <h3 class="login-box-msg">Sign In</h3>
-        <?= form_open('login'); ?>
-        <div class="form-group has-feedback">
-            <input
-                type="text"
-                name="email"
-                class="form-control sty1"
-                placeholder="Input email"/>
-        </div>
-        <div class="form-group has-feedback">
-            <input
-                type="password"
-                name="password"
-                class="form-control sty1"
-                placeholder="Password"/>
-        </div>
-        <div>
-            <div class="col-xs-8">
-                <div class="checkbox icheck">
-                    <label>
-                        <input type="checkbox"/>
-                        Remember Me
-                    </label>
-                    <a href="forgot-password" class="pull-right">
-                        <i class="fa fa-lock"></i>
-                        Forgot password?</a>
+
+        <form class="" action="login" method="post">
+            <div class="form-group has-feedback">
+                <input type="text" name="email" class="form-control sty1" placeholder="Input email"/>
+            </div>
+            <div class="form-group has-feedback">
+                <input type="password" name="password" class="form-control sty1" placeholder="Password"/>
+            </div>
+            <div>
+                <div class="col-xs-8">
+                    <div class="checkbox icheck">
+                        <label><input type="checkbox"/>Remember Me</label>
+                        <a href="forgot-password" class="pull-right">
+                            <i class="fa fa-lock"></i>
+                            Forgot password?</a>
+                    </div>
                 </div>
+                <!-- /.col -->
+                <div class="col-xs-4 m-t-1">
+                    <button onclick="location.href='dashboard';" type="submit" class="btn btn-primary btn-block btn-flat">
+                        Sign In
+                    </button>
+                </div>
+                <!-- /.col -->
             </div>
-            <!-- /.col -->
-            <div class="col-xs-4 m-t-1">
-                <button
-                    onclick="location.href='dashboard';"
-                    type="submit"
-                    class="btn btn-primary btn-block btn-flat">
-                    Sign In
-                </button>
-            </div>
-            <!-- /.col -->
-        </div>
-        <?= form_close(); ?>
+        </form>
         <div class="m-t-2">
             Don't have an account?
             <a href="register" class="text-center">Sign Up</a>
--- app/Views/register.php
+++ app/Views/register.php
@@ -17,39 +17,25 @@
             <?php endforeach ?>
         </div>
         <?php } ?>
-        <?= form_open('register'); ?>
-        <div class="form-group has-feedback">
-            <input
-                type="text"
-                name="username"
-                class="form-control sty1"
-                placeholder="Input username"/>
-        </div>
-        <div class="form-group has-feedback">
-            <input
-                type="email"
-                name="email"
-                class="form-control sty1"
-                value="<?= set_value('email') ?>"
-                placeholder="Email"/>
-        </div>
-        <div class="form-group has-feedback">
-            <input
-                type="password"
-                name="password"
-                class="form-control sty1"
-                placeholder="Password"/>
-        </div>
-        <div>
-            <!-- /.col -->
-            <div class="col-xs-4 m-t-1">
-                <button type="submit" class="btn btn-primary btn-block btn-flat">
-                    Sign UP
-                </button>
+
+        <form class="" action="register" method="post">
+            <div class="form-group has-feedback">
+                <input type="text" name="username" value="<?= set_value('username') ?>" class="form-control sty1" placeholder="Input username"/>
             </div>
-            <!-- /.col -->
-        </div>
-        <?= form_close(); ?>
+            <div class="form-group has-feedback">
+                <input type="email" name="email" value="<?= set_value('email') ?>" class="form-control sty1" placeholder="Email"/>
+            </div>
+            <div class="form-group has-feedback">
+                <input type="password" name="password" value="" class="form-control sty1" placeholder="Password"/>
+            </div>
+            <div>
+                <!-- /.col -->
+                <div class="col-xs-4 m-t-1">
+                    <button type="submit" class="btn btn-primary btn-block btn-flat"> Sign UP </button>
+                </div>
+                <!-- /.col -->
+            </div>
+        </form>
         <div class="m-t-2">
             Already have an account?
             <a href="login" class="text-center">Sign In</a>
